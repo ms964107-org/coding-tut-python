cd15a63202b7992a88e23050afaf7d8949820ce2
Martin Hsu <ms964107@gmail.com>
2022-11-18 00:33:45 -0800

    add test template and isSameTree utils

--- utils/utils.py
+++ utils/utils.py
@@ -45,3 +45,18 @@ class TreeNode(object):
             parent.left = TreeNode.build_tree(list, 2 * index + 1)
             parent.right = TreeNode.build_tree(list, 2 * index + 2)
         return parent
+    
+    def isSameTree(tree1, tree2):
+        if tree1 and tree2:
+            return tree1.val == tree2.val and TreeNode.isSameTree(tree1.left, tree2.left) and TreeNode.isSameTree(tree1.right, tree2.right)
+        return tree1 is tree2
+    
+# Tests
+class Test(object):
+    def template(tests, solution):
+        try:
+            tests(solution)
+        except:
+            print("ADVANCED TEST FAIL :(")
+        else:
+            print("ADVANCED TEST PASS :)")
